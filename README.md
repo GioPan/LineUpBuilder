8055db7b438ab09aba2429142eab86b5538e30b3
GiovanniP <giopantuso@gmail.com>
2018-01-25 21:34:01 +0100

    Included output file

--- README.md
+++ README.md
@@ -7,7 +7,10 @@ It requires two input files:
 
 Run the algorithm as:
 
-```python lub/main.py -pl path/to/players.file -par path/to/params.file```
+```python lub/main.py -l path/to/players.file -p path/to/params.file -o path/to/output.file```
+
+Arguments are optional. The defauls list of players is `data/players.dat`. The default parameters file is `data/params.dat`.
+The default output file is `output.txt`.
 
 ## Dependencies
 http://www.pyomo.org/
@@ -15,4 +18,5 @@ http://www.pyomo.org/
 https://www.gnu.org/software/glpk/
 
 ## Usage
-The code can be used freely for non-commercial purposes. However, the author retains all rights to the source code and no one may reproduce, distribute, or create derivative works.
+The code can be used freely for non-commercial and non-profit purposes and without any warranty on its correctness.
+The author retains all rights to the source code and no one may reproduce, distribute, or create derivative works.
--- lub/main.py
+++ lub/main.py
@@ -13,17 +13,29 @@ import argparse
 # Command line interpreter
 # =====================
 parser = argparse.ArgumentParser(description='K-Best LineUps Builder.')
-parser.add_argument('-pl','--playersFile',dest='playersFile', default = os.path.join("data","data/players.dat"),
-                                        help='the file containing all the players of the problem (default: data/players.dat)')
-parser.add_argument('-par','--paramsFile',dest='paramsFile', default = os.path.join("data","data/params.dat"),
+parser.add_argument('-l','--listOfPlayers',dest='playersFile', default = os.path.join("data","players.dat"),
+                                        help='the file containing the list of all the players considered (default: data/players.dat)')
+parser.add_argument('-p','--paramsFile',dest='paramsFile', default = os.path.join("data","params.dat"),
                                         help='the file containing all the parameters of the problem (default: data/params.dat)')
+parser.add_argument('-o','--outputFile',dest='outputFile', default = os.path.join("output.txt"),
+                                        help='the file where the solution should be printed (default: output.txt)')
+
 args = parser.parse_args()
 
 
 p = Generator.generate(args.playersFile,args.paramsFile)
 solutions = Branching.solve(p,p.nLineUps,p.nPlayersRequired,p.nDifferences)
-for node in solutions:
-    print 'Solution node ', node.id
-    print 'Objective ', node.objective
-    print 'Team ', node.solution
 
+# Prints the results to file
+f = open(args.outputFile,'w')
+i = 1
+for node in solutions:
+    f.write('##############\n')
+    f.write('Solution #{0} \n'.format(i))
+    f.write('Objective value : {0}\n'.format(node.objective))
+    f.write('##############\n')
+    f.write('Team :\n')
+    for player in node.solution:
+        f.write('{0}\n'.format(player))
+    i += 1
+f.close()
